1cec4543538b0909858f0e25513a9b6d5523c92e
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-23 18:20:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -454,126 +454,82 @@ def simulate_transaction(transaction, config=None):
 blockhash, last_valid_block_height = get_latest_blockhash()
 print(f"Blockhash: {blockhash}")
 print(f"Last Valid Block Height: {last_valid_block_height}")
-time.sleep(1)
+
 commitment, total_stake = get_block_commitment(5)
 print(f"Commitment: {commitment}")
 print(f"Total Stake: {total_stake}")
-time.sleep(1)
+
 blocks = get_blocks(5, 10)
 print(f"Blocks: {blocks}")
-time.sleep(1)
+
 blocksWithLimit = get_blocksWithLimit(5, 3)
 print(f"Blocks: {blocksWithLimit}")
-time.sleep(1)
+
 block_time, error = get_block_time(5)
 if error:
     print(f"Error: {error}")
 else:
     print(f"Block Time: {block_time}")
-time.sleep(1)
+
 # Using the new functions
 cluster_nodes = get_cluster_nodes()
 print(f"Cluster Nodes: {cluster_nodes}")
-time.sleep(1)
+
 epoch_info = get_epoch_info()
 print(f"Epoch Info: {epoch_info}")
-time.sleep(1)
+
 epoch_schedule = get_epoch_schedule()
 print(f"Epoch Schedule: {epoch_schedule}")
-time.sleep(1)
+
 fee_for_message = get_fee_for_message("AQABAgIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEBAQAA")
 print(f"Fee for Message: {fee_for_message}")
-time.sleep(1)
+
 first_available_block = get_first_available_block()
 print(f"First Available Block: {first_available_block}")
-time.sleep(1)
+
 # Using the new functions
 inflation_governor = get_inflation_governor()
 print(f"Inflation Governor: {inflation_governor}")
-time.sleep(1)
+
 inflation_rate = get_inflation_rate()
 print(f"Inflation Rate: {inflation_rate}")
-time.sleep(1)
+
 leader_schedule = get_leader_schedule()
 print(f"Leader Schedule: {leader_schedule}")
-time.sleep(1)
+
 # Using the new functions
 max_retransmit_slot = get_max_retransmit_slot()
 print(f"Max Retransmit Slot: {max_retransmit_slot}")
-time.sleep(1)
+
 max_shred_insert_slot = get_max_shred_insert_slot()
 print(f"Max Shred Insert Slot: {max_shred_insert_slot}")
-time.sleep(1)
+
 minimum_balance_for_rent_exemption = get_minimum_balance_for_rent_exemption(50)
 print(f"Minimum Balance For Rent Exemption: {minimum_balance_for_rent_exemption}")
-time.sleep(1)
+
 multiple_accounts = get_multiple_accounts(["vines1vzrYbzLMRdu58ou5XTby4qAqVRLmqo36NKPTg", "4fYNw3dojWmQ4dXtSGE9epjRGy9pFSx62YypT7avPYvA"])
 print(f"Multiple Accounts: {multiple_accounts}")
-time.sleep(1)
+
 # Test get_recent_performance_samples function
 recent_performance_samples = get_recent_performance_samples(5)
 print(f"Recent Performance Samples: {recent_performance_samples}")
-time.sleep(1)
+
 # Test get_recent_prioritization_fees function
 recent_prioritization_fees = get_recent_prioritization_fees(["So11111111111111111111111111111111111111112"])
 print(f"Recent Prioritization Fees: {recent_prioritization_fees}")
-time.sleep(1)
+
 # Test get_signatures_for_address function
 signatures_for_address = get_signatures_for_address("4Nd1mBQtrMJVYVfKf2PJy9NZUZdTAsp7D4xWLs4gDB4T")
 print(f"Signatures For Address: {signatures_for_address}")
-time.sleep(1)
+
 # Test get_signature_statuses function
 signature_statuses = get_signature_statuses(["4fW2UzNVA7gE1HUnBoRWcHY8YbXgN9GXPzqbS4Q4HSGyNtZRmzHnv2LVULGEX3KMk4tPXQaG9gSow27j7BqKMJhP"])
 print(f"Signature Statuses: {signature_statuses}")
-time.sleep(1)
+
 # Test get_slot function
 slot = get_slot()
 print(f"Slot: {slot}")
-time.sleep(1)
+
 # Test get_slot_leader function
 slot_leader = get_slot_leader()
 print(f"Slot Leader: {slot_leader}")
-time.sleep(1)
-# Test get_supply function
-supply = get_supply()
-print(f"Supply: {supply}")
-time.sleep(1)
-# Test get_token_account_balance function
-token_account_balance = get_token_account_balance("So11111111111111111111111111111111111111112")
-print(f"Token Account Balance: {token_account_balance}")
-time.sleep(1)
-# Test get_token_accounts_by_delegate function
-token_accounts_by_delegate = get_token_accounts_by_delegate("So11111111111111111111111111111111111111112", "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
-print(f"Token Accounts By Delegate: {token_accounts_by_delegate}")
-time.sleep(1)
-# Test get_token_accounts_by_owner function
-token_accounts_by_owner = get_token_accounts_by_owner("So11111111111111111111111111111111111111112", "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
-print(f"Token Accounts By Owner: {token_accounts_by_owner}")
-time.sleep(1)
-# Test getTokenLargestAccounts function
-token_largest_accounts = getTokenLargestAccounts("So11111111111111111111111111111111111111112")
-print(f"Token Largest Accounts: {token_largest_accounts}")
-time.sleep(1)
-# Test getTokenSupply function
-token_supply = getTokenSupply("So11111111111111111111111111111111111111112")
-print(f"Token Supply: {token_supply}")
-time.sleep(1)
-# Test getTransaction function
-transaction = getTransaction("4fW2UzNVA7gE1HUnBoRWcHY8YbXgN9GXPzqbS4Q4HSGyNtZRmzHnv2LVULGEX3KMk4tPXQaG9gSow27j7BqKMJhP")
-print(f"Transaction: {transaction}")
-time.sleep(1)
-# Test getTransactionCount function
-transaction_count = getTransactionCount()
-print(f"Transaction Count: {transaction_count}")
-time.sleep(1)
-# Test call_rpc_method function
-rpc_response = call_rpc_method("getTransactionCount")
-print(f"RPC Response: {rpc_response}")
-time.sleep(1)
-# Test get_genesis_hash function
-genesis_hash = get_genesis_hash()
-print(f"Genesis Hash: {genesis_hash}")
-time.sleep(1)
-# Test get_health function
-health = get_health()
-print(f"Health: {health}")
