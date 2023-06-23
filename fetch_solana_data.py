874f8c4a7997b7e8ee357c5d459e474c66f3cd46
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-23 11:58:16 -0400

    Delete fetch_solana_data.py

--- fetch_solana_data.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-def fetch_solana_data(node):
-    # This is a placeholder. Replace this with actual API or SDK calls.
-    url = f"https://solana.api/{node}"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    return data
