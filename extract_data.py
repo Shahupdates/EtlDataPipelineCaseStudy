0fd4baa3524fa66acc6f3a9c6d73aea745d5d562
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-24 18:27:21 -0400

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -1,3 +1,22 @@
+import requests
+import json
+import psycopg2
+import datetime
+
+url = "https://api.mainnet-beta.solana.com"
+headers = {"Content-Type": "application/json"}
+
+# PostgreSQL connection details
+conn = psycopg2.connect(
+    host='127.0.0.1',
+    port='5432',
+    database='main_database',
+    user='postgres',
+    password='postgres'
+)
+cursor = conn.cursor()
+
+
 def get_block(slot):
     try:
         payload = {
@@ -35,3 +54,64 @@ def get_block(slot):
     except Exception as e:
         print("Error:", e)
         return None, None
+
+
+def get_latest_blockhash():
+    try:
+        payload = {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "getLatestBlockhash"
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response_data = response.json()
+        if 'result' in response_data:
+            return response_data['result']['context']['slot']
+        else:
+            print("Error: No 'result' key found in the response.")
+            print(response_data)
+            return None
+    except KeyError as e:
+        print("Error: KeyError -", e)
+        return None
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+def get_magic_nfts(address):
+    magic_endpoint = "https://api-mainnet.magiceden.dev/v2/wallets/{}/tokens"
+    magic_response = requests.get(magic_endpoint.format(address))
+    if magic_response.status_code == 200:
+        tokens = magic_response.json()
+        if isinstance(tokens, list):
+            return [token.get('token') for token in tokens]
+        elif isinstance(tokens, dict):
+            return [tokens.get('token')]
+    return None
+
+
+def transform_data(data):
+    print(data[:5])  # print the first 5 items
+    filtered_data = [
+        record for record in data if datetime.datetime.fromtimestamp(int(record['timestamp'])) >
+                                   datetime.datetime.now() - datetime.timedelta(days=365 * 2)
+    ]
+    return filtered_data
+
+
+def load_data(data):
+    # Assuming you have a table named 'transactions' with appropriate columns in your PostgreSQL database
+    insert_query = "INSERT INTO public.transactions (address, timestamp, amount) VALUES (%s, to_timestamp(%s), %s)"
+    for record in data:
+        cursor.execute(insert_query, (record['address'], record['timestamp'], record['amount']))
+    conn.commit()
+
+
+if __name__ == '__main__':
+    latest_block = get_latest_blockhash()
+    if latest_block is not None:
+        transactions, _ = get_block(latest_block)
+        if transactions is not None:
+            data = transform_data(transactions)
+            load_data(data)
