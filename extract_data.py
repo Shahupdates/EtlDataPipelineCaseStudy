afeecb43dc55b99bacb0750f58a35bc303a74777
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-24 18:20:02 -0400

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -1,80 +1,20 @@
-import os
 import requests
 import json
-from pyspark.sql import SparkSession
-from subprocess import run
+import psycopg2
+import datetime
 
 url = "https://api.mainnet-beta.solana.com"
 headers = {"Content-Type": "application/json"}
 
-def get_latest_blockhash():
-    payload = {
-        "jsonrpc": "2.0",
-        "id": 1,
-        "method": "getLatestBlockhash",
-        "params": [{"commitment": "processed"}]
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-    response_data = response.json()
-    if 'result' in response_data and 'value' in response_data['result']:
-        blockhash = response_data['result']['value'].get('blockhash')
-        lastValidBlockHeight = response_data['result']['value'].get('lastValidBlockHeight')
-        if blockhash and lastValidBlockHeight:
-            return blockhash, lastValidBlockHeight
-    print("Error: Unexpected API response format.")
-    return None, None
-
-def get_addresses_from_blockhash(blockhash):
-    payload = {
-        "jsonrpc": "2.0",
-        "id": 1,
-        "method": "getBlockCommitment",
-        "params": [blockhash]
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-    response_data = response.json()
-    if 'result' in response_data:
-        return response_data['result']
-    else:
-        print(f"Error retrieving addresses from block hash {blockhash}: {response_data.get('error')}")
-        return []
-
-# ADDITION: Check if a wallet address has Magic Eden NFTs
-def has_magic_nfts(address):
-    magic_endpoint = "https://api-mainnet.magiceden.dev/v2/wallets/{}/tokens"
-    magic_response = requests.get(magic_endpoint.format(address))
-    return magic_response.status_code == 200
-
-
-def get_signatures_for_address(address, limit=1000):
-    try:
-        payload = {
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "getSignaturesForAddress",
-            "params": [
-                address,
-                {
-                    "limit": limit
-                }
-            ]
-        }
-
-        response = requests.post(url, headers=headers, data=json.dumps(payload))
-        response_data = response.json()
-
-        if 'result' in response_data:
-            return response_data['result']
-        else:
-            print(f"Error for address {address}: No 'result' key found in the response.")
-            print(response_data)
-            return None
-    except KeyError as e:
-        print(f"Error for address {address}: KeyError -", e)
-        return None
-    except Exception as e:
-        print(f"Error for address {address}:", e)
-        return None
+# PostgreSQL connection details
+conn = psycopg2.connect(
+    host='127.0.0.1',
+    port='5432',
+    database='main_database',
+    user='postgres',
+    password='postgres'
+)
+cursor = conn.cursor()
 
 
 def get_block(slot):
@@ -83,22 +23,25 @@ def get_block(slot):
             "jsonrpc": "2.0",
             "id": 1,
             "method": "getBlock",
-            "params": [slot, {"encoding": "json", "transactionDetails": "full", "rewards": False, "maxSupportedTransactionVersion": 0}]
+            "params": [slot, {"encoding": "json", "transactionDetails": "full", "rewards": False,
+                             "maxSupportedTransactionVersion": 0}]
         }
         response = requests.post(url, headers=headers, data=json.dumps(payload))
         response_data = response.json()
         if 'result' in response_data:
-            return response_data['result']
+            # Including the blockTime in the returned result
+            return response_data['result'], response_data['result'].get('blockTime')
         else:
             print("Error: No 'result' key found in the response.")
             print(response_data)
-            return None
+            return None, None
     except KeyError as e:
         print("Error: KeyError -", e)
-        return None
+        return None, None
     except Exception as e:
         print("Error:", e)
-        return None
+        return None, None
+
 
 def get_latest_blockhash():
     try:
@@ -123,63 +66,71 @@ def get_latest_blockhash():
         return None
 
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-nested_etl_dir = os.path.join(script_dir, "etlpipeline")
-os.chdir(nested_etl_dir)
+def get_magic_nfts(address):
+    magic_endpoint = "https://api-mainnet.magiceden.dev/v2/wallets/{}/tokens"
+    magic_response = requests.get(magic_endpoint.format(address))
+    if magic_response.status_code == 200:
+        tokens = magic_response.json()
+        if isinstance(tokens, list):
+            return [token.get('token') for token in tokens]
+        elif isinstance(tokens, dict):
+            return [tokens.get('token')]
+    return None
 
-# Create a SparkSession
-spark = SparkSession.builder \
-    .appName("ETL") \
-    .getOrCreate()
+
+def transform_data(data):
+    filtered_data = [
+        record for record in data if datetime.datetime.fromtimestamp(int(record['timestamp'])) >
+                                   datetime.datetime.now() - datetime.timedelta(days=365 * 2)
+    ]
+    return filtered_data
+
+
+def load_data(data):
+    # Assuming you have a table named 'transactions' with appropriate columns in your PostgreSQL database
+    insert_query = "INSERT INTO public.transactions (address, timestamp, amount) VALUES (%s, %s, %s)"
+
+    # Iterate through the transformed data and execute the insert query for each record
+    for record in data:
+        cursor.execute(insert_query, (record['address'], record['timestamp'], record['amount']))
+
+    # Commit the changes and close the cursor and connection
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 # Get the most recent block hash
-latest_blockhash, _ = get_latest_blockhash()
+latest_blockhash = get_latest_blockhash()
 
 # Unique addresses involved in the transactions
 unique_addresses = set()
 
-# If we have a recent block hash, get the addresses
+# If we have a recent block hash, get its details
 if latest_blockhash is not None:
-    addresses = get_addresses_from_blockhash(latest_blockhash)
-    print(f"Addresses for block hash: {latest_blockhash}")
+    block_data = get_block(latest_blockhash)
+    if block_data is not None:
+        # Loop through each transaction in the block and get the addresses involved in the transaction
+        addresses = [address for transaction in block_data['transactions'] for address in
+                     transaction['transaction']['message']['accountKeys']]
 
-    for address in addresses:
-        # Check if the address has Magic Eden NFTs
-        if has_magic_nfts(address):
-            unique_addresses.add(address)
+        # Process only a subset of addresses to avoid rate limits
+        addresses = addresses[:50]  # adjust this number based on your needs
 
-# Get and print signatures for each unique address
+        for address in addresses:
+            magic_nfts = get_magic_nfts(address)
+            if magic_nfts is not None:
+                unique_addresses.add(address)
+
+# Get the unique addresses
+unique_addresses = list(unique_addresses)
+
+# Transform the data
+transformed_data = transform_data(unique_addresses)
+
+# Load the transformed data into PostgreSQL
+load_data(transformed_data)
+
+# Print unique addresses
 for address in unique_addresses:
-    print(f"Address: {address}")
-    signatures = get_signatures_for_address(address)
-
-    if signatures:
-        print("Signatures:")
-        for signature in signatures:
-            print(signature)
-    print()
-
-# Run the dbt transformation
-run(["dbt", "run", "--models", "transform"])
-
-# Load the transformed data into PostgreSQL using Spark
-if unique_addresses:
-    df = spark.createDataFrame([(address,) for address in unique_addresses], ["address"])
-
-    # Define PostgreSQL connection properties
-    db_host = "127.0.0.2"
-    db_name = "postgres"
-    db_user = "postgres"
-    db_password = "postgres"
-    table_name = "transformed_addresses"
-
-    # Write the DataFrame to PostgreSQL
-    df.write.format("jdbc").options(
-        url=f"jdbc:postgresql://{db_host}/{db_name}",
-        driver="org.postgresql.Driver",
-        dbtable=table_name,
-        user=db_user,
-        password=db_password
-    ).mode("append").save()
-else:
-    print("No addresses available for the latest block hash.")
+    print(address)
