ca4c3469855422fdf885dfc0f6128a67164799ed
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-25 19:03:45 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,9 +1,8 @@
-# This file will serve as the entry point of your program.
 import asyncio
 from extract_data import process_data
 
-async def driver():
+async def main():
     await process_data()
 
 if __name__ == '__main__':
-    asyncio.run(driver())
+    asyncio.run(main())
