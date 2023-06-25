0727e92fb41483741883590a3cb206ca8725ba4b
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-25 11:03:07 -0400

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -1,3 +1,27 @@
+import subprocess
+
+import requests
+import json
+import datetime
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, row_number, to_timestamp
+from pyspark.sql.window import Window
+
+url = "https://api.mainnet-beta.solana.com"
+headers = {"Content-Type": "application/json"}
+
+# Start Spark session
+spark = SparkSession.builder \
+    .appName("Solana ETL") \
+    .config("spark.jars", "postgresql-42.6.0.jar") \
+    .getOrCreate()
+
+# PostgreSQL connection details
+jdbcUrl = "jdbc:postgresql://localhost/main_database"
+table = "public.transactions"
+properties = {"user": "postgres", "password": "postgres", "driver": "org.postgresql.Driver"}
+
+
 def get_block(slot):
     try:
         payload = {
@@ -42,3 +66,80 @@ def get_block(slot):
     except Exception as e:
         print("Error:", e)
         return None, None
+
+
+def get_latest_blockhash():
+    try:
+        payload = {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "getLatestBlockhash"
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response_data = response.json()
+        if 'result' in response_data:
+            return response_data['result']['context']['slot']
+        else:
+            print("Error: No 'result' key found in the response.")
+            print(response_data)
+            return None
+    except KeyError as e:
+        print("Error: KeyError -", e)
+        return None
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+def get_magic_nfts(address):
+    magic_endpoint = "https://api-mainnet.magiceden.dev/v2/wallets/{}/tokens"
+    magic_response = requests.get(magic_endpoint.format(address))
+    if magic_response.status_code == 200:
+        tokens = magic_response.json()
+        if isinstance(tokens, list):
+            return [token.get('token') for token in tokens]
+        elif isinstance(tokens, dict):
+            return [tokens.get('token')]
+    return None
+
+"""
+def transform_data(data):
+    print(data[:5])  # print the first 5 items
+    filtered_data = [
+        {
+            'address': record['address'],
+            'amount': record['amount'],
+            'timestamp': datetime.datetime.strptime(record['timestamp'], '%Y-%m-%d %H:%M:%S')
+        }
+        for record in data
+        if datetime.datetime.strptime(record['timestamp'], '%Y-%m-%d %H:%M:%S') > datetime.datetime.now() - datetime.timedelta(days=365 * 2)
+    ]
+    return filtered_data
+"""
+
+def load_data(data):
+    df = spark.createDataFrame(data, ["address", "amount", "timestamp"])
+
+    # Cast the timestamp column to the appropriate data type
+    df = df.withColumn("timestamp", to_timestamp(col("timestamp")))
+
+    # Deduplicate the data based on the address and timestamp columns
+    window = Window.partitionBy("address", "timestamp").orderBy(col("amount").desc())
+    deduplicated_df = df.withColumn("row_number", row_number().over(window)).where(col("row_number") == 1).drop("row_number")
+
+    deduplicated_df.write.jdbc(url=jdbcUrl, table=table, mode="append", properties=properties)
+    print('Data loaded successfully.')
+
+def run_dbt_transformation():
+    command = "dbt run --models +transformations.transform_data"
+    subprocess.run(command, shell=True)
+
+if __name__ == '__main__':
+    latest_block = get_latest_blockhash()
+    if latest_block is not None:
+        transactions, _ = get_block(latest_block)
+        if transactions is not None:
+            load_data(transactions)
+            run_dbt_transformation()
+
+    spark.stop()
