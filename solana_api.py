1bd7cc255ab4048fc3ef727e06f0232f60703783
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-25 21:25:59 -0400

    Update solana_api.py

--- solana_api.py
+++ solana_api.py
@@ -1,17 +1,49 @@
 import asyncio
 
+import aiohttp
 import requests
 import json
 import datetime
 
-from extract_data import ignored_accounts, get_magic_nfts_async
-
 url = "https://api.mainnet-beta.solana.com"
 headers = {"Content-Type": "application/json"}
+
+ignored_accounts = set([
+    'SysvarC1ock11111111111111111111111111111111',
+    'SysvarS1otHashes111111111111111111111111111',
+    'ComputeBudget111111111111111111111111111111',
+    'Vote111111111111111111111111111111111111111',
+    '11111111111111111111111111111111'
+])
 
 class SolanaAPI:
     def __init__(self, session):
         self.session = session
+        self.magic_nfts_cache = {}
+
+    async def get_magic_nfts_async(self, i, address):
+        # If address already checked, fetch the response from cache
+        if address in self.magic_nfts_cache:
+            return i, self.magic_nfts_cache[address]
+
+        magic_endpoint = f"https://api-mainnet.magiceden.dev/v2/wallets/{address}/tokens"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(magic_endpoint) as resp:
+                if resp.status == 200:
+                    tokens = await resp.json()
+                    # Store the response in cache
+                    if isinstance(tokens, list) and tokens:
+                        self.magic_nfts_cache[address] = [token.get('token') for token in tokens]
+                        print(f"Magic Eden NFT(s) found for account {address}.")
+                        return i, self.magic_nfts_cache[address]
+                    elif isinstance(tokens, dict) and tokens.get('token'):
+                        self.magic_nfts_cache[address] = [tokens.get('token')]
+                        print(f"Magic Eden NFT found for account {address}.")
+                        return i, self.magic_nfts_cache[address]
+
+                    # Store negative response in cache
+                    self.magic_nfts_cache[address] = None
+        return i, None
 
     async def get_block(self, slot):
         print(f"Getting block for slot: {slot}")
@@ -33,7 +65,7 @@ class SolanaAPI:
                     result = response_data['result']
                     transactions = result['transactions']
                     for transaction in transactions:
-                        transaction_data = await self.process_transaction_data(transaction, result)  # Add 'await' here
+                        transaction_data = await self.process_transaction_data(transaction, result)
                         if transaction_data:
                             transactions_list.append(transaction_data)
                     return transactions_list, result.get('blockTime')
@@ -71,16 +103,17 @@ class SolanaAPI:
             print("Error:", e)
             return None
 
-    def process_transaction_data(self, transaction, result):
+    async def process_transaction_data(self, transaction, result):
         message = transaction['transaction']['message']
         meta = transaction['meta']
         tasks = []
         for i, account in enumerate(message['accountKeys']):
             if account not in ignored_accounts:  # Exclude ignored accounts
                 print(f"Checking Magic Eden NFTs for account {account}")
-                tasks.append(get_magic_nfts_async(i, account))
-        for future in asyncio.as_completed(tasks):
-            i, magic_nfts = await future
+                tasks.append(self.get_magic_nfts_async(i,
+                                                       account))  # I assumed this is an async function. If not, you should make it async.
+        results = await asyncio.gather(*tasks)  # wait for all tasks to finish
+        for i, magic_nfts in enumerate(results):
             if magic_nfts:
                 account = message['accountKeys'][i]
                 print(f"Finished checking Magic Eden NFTs for account {account}")
@@ -99,3 +132,5 @@ class SolanaAPI:
                 }
                 return transaction_dict
         return None
+
+
