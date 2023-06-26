a84956810e3536cd064b9ef0598ef966c903e22d
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-25 21:30:43 -0400

    Update extract_data.py
     I've added a while True: loop that will keep the script running indefinitely. After each iteration, the script will pause for 60 seconds (you can adjust this interval as needed) to avoid excessively polling your data source.

--- extract_data.py
+++ extract_data.py
@@ -2,6 +2,7 @@ import aiohttp
 import asyncio
 import json
 import datetime
+import time
 from spark_operations import SparkOperations
 from solana_api import SolanaAPI
 from magic_eden_api import MagicEdenAPI
@@ -14,21 +15,27 @@ ignored_accounts = set([
     '11111111111111111111111111111111'
 ])
 
+
 async def process_data():
     async with aiohttp.ClientSession() as session:
         solana_api = SolanaAPI(session)
         magic_eden_api = MagicEdenAPI(session)
         spark_operations = SparkOperations()
 
-        latest_block = await solana_api.get_latest_blockhash()
-        if latest_block is not None:
-            transactions, _ = await solana_api.get_block(latest_block)
-            if transactions is not None:
-                deduplicated_df = spark_operations.load_data(transactions)
+        while True:  # Loop to continuously process data
+            latest_block = await solana_api.get_latest_blockhash()
+            if latest_block is not None:
+                transactions, _ = await solana_api.get_block(latest_block)
+                if transactions is not None:
+                    deduplicated_df = spark_operations.load_data(transactions)
 
-                spark_operations.calculate_dau(deduplicated_df)
-                spark_operations.calculate_daily_transaction_volume(deduplicated_df)
-                spark_operations.run_dbt_transformation()
+                    spark_operations.calculate_dau(deduplicated_df)
+                    spark_operations.calculate_daily_transaction_volume(deduplicated_df)
+                    spark_operations.run_dbt_transformation()
+
+            # Sleep for a certain amount of time to prevent excessive polling.
+            # You can adjust the sleep interval according to your needs.
+            time.sleep(60)
 
         spark_operations.stop_spark()
 
